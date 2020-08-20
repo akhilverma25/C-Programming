f797052330a58c55321044a8199a6ff959037d84
Akhil Verma <60506695+akhilverma25@users.noreply.github.com>
2020-08-20 06:09:01 +0530

    Add files via upload

---
+++ DEMOTION.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a=3.5, b=3.9;
+printf("Value of a is%d", a);
+printf("Value of b is%d", b);
+getch();
+}
---
+++ FLOAT.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a=5.32489187;
+clrscr();
+printf("%f", a);
+printf("\n%.3f",a);
+printf("\n%.5f",a);
+getch();
+}
