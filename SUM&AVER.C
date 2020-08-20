80a7b40dbb778e3b2f1e2ff5c6ab460a32bf77ad
Akhil Verma <60506695+akhilverma25@users.noreply.github.com>
2020-08-20 06:09:15 +0530

    Add files via upload

---
+++ SUM&AVER.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+float a=10.239,b=20.373;
+int c;
+c=a+b;
+printf("Sum of two numbers is %f",a+b);
+printf("Average of two numbers is %f",c/2);
+getch();
+}
+
